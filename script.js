ff136298a8f598528170c08c2600bff76afd237b
Alagu Raja <108890879+alagurajabar@users.noreply.github.com>
2025-05-01 06:06:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 window.onload = function() {
     const images = [
-        './images/1.jpeg',
-        './images/2.jpeg',
-        './images/3.jpeg',
-        './images/4.jpeg',
-        './images/5.jpeg',
-        './images/6.jpeg',
-        './images/7.jpeg'
+        './images/1.jpg',
+        './images/2.jpg',
+        './images/3.jpg',
+     
     ];
 
     // Get a random index from the images array
